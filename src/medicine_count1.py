ac7584fbf4dbccf1871d001eee3d28939a7a4726
Trung Nguyen <32110646+trungnguyencs@users.noreply.github.com>
2019-02-18 16:11:01 -0800

    fixed for duplicated prescribers

--- src/medicine_count1.py
+++ src/medicine_count1.py
@@ -8,16 +8,20 @@ def main():
         lines = f.readlines()
         for i in range(1, len(lines)):
             line = lines[i].strip().split(',')
-            try:
-                drug_name, cost = line[3], int(round(float(line[4]),0)) # name is in the 3rd, cost if in the 4th column
-                sum_total_cost(drug_name.upper(), cost, drug_dict)    
-            except: 
-                print('Line %d is corrupt!' %(i))
+            # try:
+                # name is in the 3rd, cost if in the 4th column
+            pres_last = line[1]
+            pres_first = line[2]
+            drug_name = line[3].upper()
+            cost = int(round(float(line[4]))) 
+            sum_total_cost(pres_last, pres_first, drug_name, cost, drug_dict)    
+            # except: 
+            #     print('Line %d is corrupt!' %(i))
         ord_dict = ord_cost(drug_dict)
     with open(f_out, 'w') as file:  
         file.write('drug_name,num_prescriber,total_cost\n')
         for k,v in ord_dict:
-            file.write(str(k) + ',' + str(v[0]) + ',' + str(v[1]) + '\n')
+            file.write(str(k) + ',' + str(len(v[0])) + ',' + str(v[1]) + '\n')
 
 def get_file_path(): 
     if len (sys.argv) != 3: # check if correct number of arguments is provided  
@@ -30,17 +34,15 @@ def get_file_path():
         sys.exit()
     return f_in, f_out
 
-def sum_total_cost(drug_name, cost, drug_dict):
+def sum_total_cost(pres_last, pres_first, drug_name, cost, drug_dict):
     if drug_name in drug_dict:
-        count_cost = drug_dict[drug_name]
-        count, total_cost = count_cost[0], count_cost[1]
-        drug_dict[drug_name] = [count + 1, total_cost + cost]
+        drug_dict[drug_name][1] += cost 
+        drug_dict[drug_name][0][(pres_first, pres_last)] = None 
     else: 
-        drug_dict[drug_name] = [1, cost]
-    return 
+        drug_dict[drug_name] = [{(pres_first, pres_last): None}, cost]
 
 def ord_cost(drug_dict, desc=True):  
-    drug_lst = [(drug, [count, cost]) for drug, [count, cost] in drug_dict.items()]
+    drug_lst = [(drug, [pres_dict, cost]) for drug, [pres_dict, cost] in drug_dict.items()]
     return sorted(drug_lst, key=lambda x: x[1][1], reverse=desc)
 
 if __name__ == '__main__':  
